a6bbf40be9a44f09b1e6c65999d372b3c175d076
saki4510t <saki4510t@gmail.com>
2018-04-25 02:12:04 +0900

    long poll, join、diconnectの処理を追加

--- app/src/main/java/com/serenegiant/janus/JanusRESTRTCClient.java
+++ app/src/main/java/com/serenegiant/janus/JanusRESTRTCClient.java
@@ -10,12 +10,20 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.internal.bind.DateTypeAdapter;
+import com.serenegiant.janus.request.Attach;
 import com.serenegiant.janus.request.Creator;
+import com.serenegiant.janus.request.Destroy;
+import com.serenegiant.janus.request.Message;
+import com.serenegiant.janus.response.Event;
+import com.serenegiant.janus.response.Plugin;
+import com.serenegiant.janus.response.SendResponse;
 import com.serenegiant.janus.response.ServerInfo;
 import com.serenegiant.janus.response.Session;
 import com.serenegiant.utils.HandlerThreadHandler;
 
 import org.appspot.apprtc.AppRTCClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.webrtc.IceCandidate;
 import org.webrtc.SessionDescription;
 
@@ -60,12 +68,15 @@ public class JanusRESTRTCClient implements AppRTCClient {
 	private final RandomString mRandomString = new RandomString();
 	private final WeakReference<Context> mWeakContext;
 	private VideoRoom mJanus;
+	private LongPoll mLongPoll;
 	private Call<?> mCurrentCall;
+	private Call<Event> mLongPollCall;
 	private RoomConnectionParameters connectionParameters;
 	private Handler handler;
 	private ConnectionState roomState;
 	private ServerInfo mServerInfo;
 	private Session mSession;
+	private Plugin mPlugin;
 
 	public JanusRESTRTCClient(@NonNull final Context context,
 		final SignalingEvents events,
@@ -73,6 +84,7 @@ public class JanusRESTRTCClient implements AppRTCClient {
 
 		mWeakContext = new WeakReference<>(context);
 		handler = HandlerThreadHandler.createHandler(TAG);
+		roomState = ConnectionState.NEW;
 		initAsync(baseUrl);
 	}
 	
@@ -208,6 +220,7 @@ public class JanusRESTRTCClient implements AppRTCClient {
 	
 	@Override
 	public void disconnectFromRoom() {
+		cancelCall();
 		handler.post(new Runnable() {
 			@Override
 			public void run() {
@@ -251,6 +264,14 @@ public class JanusRESTRTCClient implements AppRTCClient {
 					Log.w(TAG, e);
 				}
 			}
+			if ((mLongPollCall != null) && !mLongPollCall.isCanceled()) {
+				try {
+					mLongPollCall.cancel();
+				} catch (final Exception e) {
+					mLongPollCall = null;
+					Log.w(TAG, e);
+				}
+			}
 		}
 	}
 
@@ -259,62 +280,45 @@ public class JanusRESTRTCClient implements AppRTCClient {
 		handler.post(new Runnable() {
 			@Override
 			public void run() {
-				mJanus = setupRetrofit(setupHttpClient(), baseUrl).create(VideoRoom.class);
+				mJanus = setupRetrofit(
+					setupHttpClient(HTTP_READ_TIMEOUT_MS, HTTP_WRITE_TIMEOUT_MS),
+					baseUrl).create(VideoRoom.class);
+				mLongPoll = setupRetrofit(
+					setupHttpClient(HTTP_READ_TIMEOUT_MS_LONG_POLL, HTTP_WRITE_TIMEOUT_MS),
+					baseUrl).create(LongPoll.class);
 				final Call<ServerInfo> call = mJanus.getInfo();
 				setCall(call);
-				call.enqueue(new Callback<ServerInfo>() {
-					@Override
-					public void onResponse(@NonNull final Call<ServerInfo> call, @NonNull final Response<ServerInfo> response) {
+				try {
+					final Response<ServerInfo> response = call.execute();
+					if (DEBUG) Log.v(TAG, "initAsync#onResponse:" + response);
+					setCall(null);
+					if (response.isSuccessful() && (response.body() != null)) {
+						mServerInfo = response.body();
+						if (DEBUG) Log.v(TAG, "initAsync#onResponse:" + mServerInfo);
+					} else {
+						reportError("initAsync: unexpected response " + response);
+					}
+				} catch (IOException e) {
+					setCall(null);
+					reportError(e.getMessage());
+				}
+				if (mServerInfo != null) {
+					final Call<Session> createCall = mJanus.create(new Creator(mRandomString.get(12)));
+					setCall(call);
+					try {
+						final Response<Session> response = createCall.execute();
 						if (DEBUG) Log.v(TAG, "initAsync#onResponse:" + response);
 						setCall(null);
 						if (response.isSuccessful() && (response.body() != null)) {
-							mServerInfo = response.body();
-							if (DEBUG) Log.v(TAG, "initAsync#onResponse:" + mServerInfo);
-							createSession();
-						}
-					}
-					
-					@Override
-					public void onFailure(@NonNull final Call<ServerInfo> call, @NonNull final Throwable t) {
-						if (DEBUG) Log.v(TAG, "initAsync#onFailure" + t);
-						setCall(null);
-						reportError(t.getMessage());
-					}
-				});
-			}
-		});
-	}
-	
-	private void createSession() {
-		if (DEBUG) Log.v(TAG, "createSession:");
-		handler.post(new Runnable() {
-			@Override
-			public void run() {
-				final Call<Session> call = mJanus.create(new Creator(mRandomString.get(12)));
-				setCall(call);
-				call.enqueue(new retrofit2.Callback<Session>() {
-					@Override
-					public void onResponse(@NonNull final Call<Session> call,
-						@NonNull final Response<Session> response) {
-					
-						if (DEBUG) Log.v(TAG, "createSession#onResponse:" + response);
-						setCall(null);
-						if (response.isSuccessful() && (response.body() != null)) {
 							mSession = response.body();
-							if (DEBUG) Log.v(TAG, "createSession#onResponse:" + mSession);
+							if (DEBUG) Log.v(TAG, "initAsync#onResponse:" + mSession);
 							// FIXME 未実装
 						}
-					}
-					
-					@Override
-					public void onFailure(@NonNull final Call<Session> call,
-						@NonNull final Throwable t) {
-					
-						if (DEBUG) Log.v(TAG, "createSession#onFailure" + t);
+					} catch (final IOException e) {
 						setCall(null);
-						reportError(t.getMessage());
+						reportError(e.getMessage());
 					}
-				});
+				}
 			}
 		});
 	}
@@ -322,6 +326,22 @@ public class JanusRESTRTCClient implements AppRTCClient {
 	// Connects to room - function runs on a local looper thread.
 	private void connectToRoomInternal() {
 		if (DEBUG) Log.v(TAG, "connectToRoomInternal:");
+		if (mSession != null) {
+			roomState = ConnectionState.NEW;
+			attach();
+			if (mPlugin != null) {
+				// FIXME 未実装
+				longPoll();
+				try {
+					join();
+				} catch (final IOException e) {
+					cancelCall();
+					reportError(e.getMessage());
+				}
+			}
+		} else {
+			reportError("session is not ready/already disconnected");
+		}
 //		String connectionUrl = getConnectionUrl(connectionParameters);
 //		Log.d(TAG, "Connect to room: " + connectionUrl);
 //		roomState = ConnectionState.NEW;
@@ -352,35 +372,130 @@ public class JanusRESTRTCClient implements AppRTCClient {
 	// Disconnect from room and send bye messages - runs on a local looper thread.
 	private void disconnectFromRoomInternal() {
 		if (DEBUG) Log.v(TAG, "disconnectFromRoomInternal:state=" + roomState);
-//		if (roomState == ConnectionState.CONNECTED) {
-//			if (DEBUG) Log.d(TAG, "Closing room.");
+		cancelCall();
+		if (roomState == ConnectionState.CONNECTED) {
+			if (DEBUG) Log.d(TAG, "Closing room.");
 //			sendPostMessage(WebSocketRTCClient.MessageType.LEAVE, leaveUrl, null);
-//		}
-//		roomState = WebSocketRTCClient.ConnectionState.CLOSED;
+		}
+		if (mSession != null) {
+			final Destroy destroy = new Destroy(mSession);
+			final Call<Void> call = mJanus.destroy(mSession.id(), destroy);
+			try {
+				call.execute();
+			} catch (final IOException e) {
+				reportError(e.getMessage());
+			}
+			setCall(null);
+		}
+		mPlugin = null;
+		mSession = null;
+		mServerInfo = null;
+		roomState = ConnectionState.CLOSED;
+		mJanus = null;
+		mLongPollCall = null;
 //		if (wsClient != null) {
 //			wsClient.disconnect(true);
 //		}
 	}
 	
 //--------------------------------------------------------------------
-	private void reportError(final String errorMessage) {
-		Log.e(TAG, errorMessage);
-		handler.post(new Runnable() {
+	private void longPoll() {
+		if (DEBUG) Log.v(TAG, "longPoll:");
+		final Call<Event> call = mLongPoll.getEvent(mSession.id());
+		synchronized (mSync) {
+			mLongPollCall = call;
+		}
+		call.enqueue(new Callback<Event>() {
 			@Override
-			public void run() {
-				if (roomState != ConnectionState.ERROR) {
-					roomState = ConnectionState.ERROR;
-//					events.onChannelError(errorMessage);	// FIXME
+			public void onResponse(@NonNull final Call<Event> call, @NonNull final Response<Event> response) {
+				if (DEBUG) Log.v(TAG, "longPoll:onResponse=" + response
+					+ "\nevent=" + response.body());
+				synchronized (mSync) {
+					mLongPollCall = null;
 				}
+				longPoll();
+			}
+			
+			@Override
+			public void onFailure(@NonNull final Call<Event> call, @NonNull final Throwable t) {
+				if (DEBUG) Log.v(TAG, "longPoll:onFailure=" + t);
+				synchronized (mSync) {
+					mLongPollCall = null;
+				}
+				reportError(t.getMessage());
 			}
 		});
+	}
+	
+	private void attach() {
+		if (DEBUG) Log.v(TAG, "attach:");
+		final Attach attach = new Attach(mSession, "janus.plugin.videoroom");
+		final Call<Plugin> call = mJanus.attach(mSession.id(), attach);
+		setCall(call);
+		try {
+			final Response<Plugin> response = call.execute();
+			if (DEBUG) Log.v(TAG, "attach#onResponse:" + response);
+			setCall(null);
+			if (response.isSuccessful() && (response.body() != null)) {
+				mPlugin = response.body();
+				if (DEBUG) Log.v(TAG, "attach#onResponse:" + mPlugin);
+			} else {
+				reportError("attach:unexpected response " + response);
+			}
+		} catch (final IOException e) {
+			setCall(null);
+			reportError(e.getMessage());
+		}
+	}
+
+	private void join() throws IOException {
+		if (DEBUG) Log.v(TAG, "join:");
+		final JSONObject json = new JSONObject();
+		try {
+			json.put("request", "join")
+			.put("room", "room")
+			.put("ptype", "publisher")
+			.put("display", "android");
+		} catch (final JSONException e) {
+			throw new IOException(e);
+		}
+		final Response<SendResponse> response = sendInternal(json);
+		if (DEBUG) Log.v(TAG, "join:" + response + "\n" + response.body());
+	}
+	
+	private final Response<SendResponse> sendInternal(@NonNull final JSONObject body)
+		throws IOException {
+		
+		final Message message = new Message(mSession, mPlugin, body);
+		final Call<SendResponse> call = mJanus.send(mSession.id(), mPlugin.id(), message);
+		setCall(call);
+		return call.execute();
+	}
+
+	private void reportError(final String errorMessage) {
+		Log.e(TAG, errorMessage);
+		try {
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					if (roomState != ConnectionState.ERROR) {
+						roomState = ConnectionState.ERROR;
+	//					events.onChannelError(errorMessage);	// FIXME
+					}
+				}
+			});
+		} catch (final Exception e) {
+			// ignore, will be already released.
+		}
 	}
 	
 	/**
 	 * Janus-gatewayサーバーとの通信用のOkHttpClientインスタンスの初期化処理
 	 * @return
 	 */
-	private OkHttpClient setupHttpClient() {
+	private OkHttpClient setupHttpClient(
+		final long readTimeoutMs, final long writeTimeoutMs) {
+	
 		if (DEBUG) Log.v(TAG, "setupHttpClient:");
 
 		final OkHttpClient.Builder builder = new OkHttpClient.Builder();
@@ -401,8 +516,8 @@ public class JanusRESTRTCClient implements AppRTCClient {
 				}
 			})
 			.connectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS)	// 接続タイムアウト
-			.readTimeout(HTTP_READ_TIMEOUT_MS, TimeUnit.MILLISECONDS)		// 読み込みタイムアウト
-			.writeTimeout(HTTP_WRITE_TIMEOUT_MS, TimeUnit.MILLISECONDS);	// 書き込みタイムアウト
+			.readTimeout(readTimeoutMs, TimeUnit.MILLISECONDS)		// 読み込みタイムアウト
+			.writeTimeout(writeTimeoutMs, TimeUnit.MILLISECONDS);	// 書き込みタイムアウト
 		
 		// ログ出力設定
 		if (DEBUG) {
