569f569b9e41ce985034ea0b8568846d5f9f562f
saki4510t <saki4510t@gmail.com>
2018-04-25 00:16:52 +0900

    Retrofit2でのJanus-gatewayへのアクセスのための定数クラスを追加

---
+++ app/src/main/java/com/serenegiant/janus/Const.java
@@ -0,0 +1,10 @@
+package com.serenegiant.janus;
+
+public class Const {
+	/** Janus-gatewayサーバーとの接続タイムアウト設定[ミリ秒] */
+	/*package*/ static final long HTTP_CONNECT_TIMEOUT_MS = 3000;
+	/** Janus-gatewayサーバーからの読み込みタイムアウト設定[ミリ秒] */
+	/*package*/ static final long HTTP_READ_TIMEOUT_MS = 3000;
+	/** Janus-gatewayサーバーへの書き込みタイムアウト設定[ミリ秒] */
+	/*package*/ static final long HTTP_WRITE_TIMEOUT_MS = 3000;
+}
