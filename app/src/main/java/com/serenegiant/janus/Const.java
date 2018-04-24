094a275a6266565711abecfa620242fd8a015ffa
saki4510t <saki4510t@gmail.com>
2018-04-25 02:11:28 +0900

    long poll用の待機時間を変更、Janus側は30秒らしいので45秒に設定

--- app/src/main/java/com/serenegiant/janus/Const.java
+++ app/src/main/java/com/serenegiant/janus/Const.java
@@ -5,6 +5,7 @@ public class Const {
 	/*package*/ static final long HTTP_CONNECT_TIMEOUT_MS = 3000;
 	/** Janus-gatewayサーバーからの読み込みタイムアウト設定[ミリ秒] */
 	/*package*/ static final long HTTP_READ_TIMEOUT_MS = 3000;
+	/*package*/ static final long HTTP_READ_TIMEOUT_MS_LONG_POLL = 45000;
 	/** Janus-gatewayサーバーへの書き込みタイムアウト設定[ミリ秒] */
 	/*package*/ static final long HTTP_WRITE_TIMEOUT_MS = 3000;
 }
