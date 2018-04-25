dd85cde6946e3242cd25cf497ad6d3ad663bbdb9
saki4510t <saki4510t@gmail.com>
2018-04-25 16:32:38 +0900

    add Join request parameter class

---
+++ app/src/main/java/com/serenegiant/janus/request/Join.java
@@ -0,0 +1,15 @@
+package com.serenegiant.janus.request;
+
+public class Join {
+	public final String request;
+	public final int room;
+	public final String ptype;
+	public final String display;
+	
+	public Join(final int room, final String display) {
+		this.request = "join";
+		this.room = room;
+		this.ptype = "publisher";
+		this.display = display;
+	}
+}
