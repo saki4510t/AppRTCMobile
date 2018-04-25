57feb90f4645fa1183f670cb184d29513d2d381b
saki4510t <saki4510t@gmail.com>
2018-04-25 23:21:26 +0900

    Room情報を保持するためのクラスを追加

---
+++ app/src/main/java/com/serenegiant/janus/Room.java
@@ -0,0 +1,28 @@
+package com.serenegiant.janus;
+
+import android.support.annotation.NonNull;
+
+import com.serenegiant.janus.response.Plugin;
+import com.serenegiant.janus.response.Session;
+
+import java.math.BigInteger;
+
+public class Room {
+	@NonNull
+	public final BigInteger sessionId;
+	@NonNull
+	public final BigInteger pluginId;
+	
+	public String state;
+	public BigInteger id;
+
+	public Room(@NonNull final Session session, @NonNull final Plugin plugin) {
+		this.sessionId = session.id();
+		this.pluginId = plugin.id();
+	}
+	
+	@NonNull
+	public String clientId() {
+		return id != null ? id.toString(16) : "";
+	}
+}
