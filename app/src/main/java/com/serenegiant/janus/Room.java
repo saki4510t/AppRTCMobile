6248297b690b35bd4100c2bb8e29eaab5dea3cac
saki4510t <saki4510t@gmail.com>
2018-04-28 14:41:57 +0900

    Publishersの更新＆変更チェックを追加

--- app/src/main/java/com/serenegiant/janus/Room.java
+++ app/src/main/java/com/serenegiant/janus/Room.java
@@ -1,11 +1,16 @@
 package com.serenegiant.janus;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.serenegiant.janus.response.Plugin;
+import com.serenegiant.janus.response.PublisherInfo;
 import com.serenegiant.janus.response.Session;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Room {
 	/**
@@ -27,8 +32,13 @@ public class Room {
 	
 	/**
 	 * クライアントID
+	 * EventRoom.plugindata.data.idの値
+	 * publisherとしての自id
 	 */
-	public BigInteger id;
+	public BigInteger publisherId;
+	
+	private final List<PublisherInfo> publishers
+		= new ArrayList<>();
 
 	public Room(@NonNull final Session session, @NonNull final Plugin plugin) {
 		this.sessionId = session.id();
@@ -36,11 +46,34 @@ public class Room {
 	}
 	
 	/**
-	 * クライアントIdを取得
-	 * @return
+	 * Publisherをセット
+	 * @param newPublishers
+	 * @return 追加または削除されたPublisherのリスト
 	 */
 	@NonNull
-	public String clientId() {
-		return id != null ? id.toString() : "";
+	public List<PublisherInfo> updatePublisher(
+		@Nullable final PublisherInfo[] newPublishers) {
+
+		final List<PublisherInfo> result;
+		if (newPublishers != null) {
+			result = Arrays.asList(newPublishers);
+			final List<PublisherInfo> src = Arrays.asList(newPublishers);
+			
+			synchronized (this.publishers) {
+				// 既にRoomに登録されているPublisherを除く=未登録分
+				result.removeAll(this.publishers);
+				// 既にRoomに登録されているものから新しいPublisherを除く=削除分
+				this.publishers.removeAll(src);
+				result.addAll(this.publishers);
+				this.publishers.clear();
+				this.publishers.addAll(src);
+			}
+		} else {
+			synchronized (this.publishers) {
+				result = new ArrayList<>(this.publishers);
+				this.publishers.clear();
+			}
+		}
+		return result;
 	}
 }
